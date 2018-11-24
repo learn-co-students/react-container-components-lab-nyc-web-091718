fa27f99a1d977f5bb8c5f3ff66361a9c5a8b71ef
github username <github email address>
2018-11-24 09:47:13 -0600

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,16 @@
-// Code MovieReviews Here
+import React from 'react'// Code MovieReviews Here
+
+export default function MovieReviews(props) {
+  return (
+    <div className="review">
+      <header>
+        <a className="review-link" /*href={link.url}*/>
+          {/*headline*/}
+        </a>
+        <br/>
+        <span className="author">{/*byline*/}</span>
+      </header>
+      <blockquote>{/*summary_short*/}</blockquote>
+    </div>
+  );
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -3,7 +3,17 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'+ `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <h2>Movie Review By Search:</h2>
+        <MovieReviews/>
+      </div>
+    );
+  }
+}
+
+export default SearchableMovieReviewsContainer
